aade3aab8510d8fb65fd8cb16b1046a3540ab81a
Encode42 <me@encode42.dev>
2022-01-03 20:45:47 -0500

    Better way of deploying pages

---
+++ public/demo/src/index.js
@@ -0,0 +1,82 @@
+const elements = {
+    "button": {},
+    "text": {
+        "result": {}
+    }
+};
+
+let isFirefix;
+
+window.addEventListener("load", () => {
+    elements.button.fileInput = document.getElementById("file-input");
+    elements.text.highlighting = document.getElementById("highlight-status");
+    elements.text.result.structure = document.getElementById("result-structure");
+    elements.text.result.overview = document.getElementById("result-overview");
+
+    // Initial result state
+    prepareResult("No file selected.");
+
+    // Initialize file input
+    elements.button.fileInput.value = null;
+
+    // Sneaky Firefox detection
+    isFirefix = navigator.userAgent.indexOf("Firefox") > 0;
+
+    elements.button.fileInput.addEventListener("change",  event => {
+        const worker = new Worker("src/loadSong.js");
+
+        // Load the song
+        prepareResult("Loading...");
+        worker.postMessage({
+            "file": event.target.files[0]
+        });
+
+        worker.addEventListener("message", event => {
+            // Fill result table
+            for (const overview of event.data.overviews) {
+                const row = document.createElement("tr");
+
+                const key = document.createElement("td");
+                key.innerHTML = `<strong>${overview[0]}</strong>`;
+
+                const value = document.createElement("td");
+                value.innerHTML = overview[1] === "" ? "None" : overview[1];
+
+                row.append(key);
+                row.append(value);
+
+                elements.text.result.overview.append(row);
+            }
+
+            // Set structure text
+            if (isFirefix) {
+                elements.text.result.structure.innerHTML = event.data.structureText;
+            } else {
+                const highlightWorker = new Worker("src/highlight.js", {
+                    "type": "module"
+                });
+
+                elements.text.highlighting.classList.add("visible");
+
+                // Highlight the block
+                highlightWorker.postMessage({
+                    "code": event.data.structureText
+                });
+
+                highlightWorker.addEventListener("message", event => {
+                    elements.text.result.structure.innerHTML = event.data.code;
+                    elements.text.highlighting.classList.remove("visible");
+                });
+            }
+        });
+    });
+});
+
+/**
+ * Prepare the result code block with a placeholder message.
+ * @param placeholder Message to display
+ */
+function prepareResult(placeholder) {
+    elements.text.result.overview.innerHTML = null;
+    elements.text.result.structure.innerHTML = placeholder;
+}
---
+++ public/demo/src/loadSong.js
@@ -0,0 +1,88 @@
+self.importScripts("../../dist/index.js");
+
+self.addEventListener("message", async event => {
+    // Load the song
+    const song = NBSjs.Song.fromArrayBuffer(await event.data.file.arrayBuffer());
+
+    // Remove undefined notes and empty layers
+    const newLayers = [];
+    for (let i = 0; i < song.layers.length; i++) {
+        const layer = song.layers[i];
+
+        // Check for empty notes
+        const newNotes = [];
+        for (const note of layer.notes) {
+            if (note !== undefined) {
+                newNotes.push(note);
+            }
+        }
+
+        layer.notes = newNotes;
+
+        // Check for empty layers
+        if (layer.notes.length > 0) {
+            newLayers.push(layer);
+        }
+    }
+
+    song.layers = newLayers;
+
+    // Display the instruments first
+    const data = {
+        "structureText": `Instruments: ${JSON.stringify(song.instruments, null, 4)}\n\n`,
+        "overviews": [[
+            "NBS version",
+            song.nbsVersion
+        ], [
+            "Song name",
+            song.name
+        ], [
+            "Song author",
+            song.author
+        ], [
+            "Song description",
+            song.description
+        ], [
+            "Total layers",
+            song.layers.length
+        ], [
+            "Total notes",
+            song.size
+        ], [
+            "Custom instruments",
+            song.instruments.map(i => {
+                if (!i.builtIn) {
+                    return i.name;
+                }
+
+                return null;
+            }).filter(Boolean)
+                .join(", ")
+        ]]
+    };
+
+    // Stringify the song structure
+    const cache = [];
+    data.structureText += "Song: " + JSON.stringify(song, (key, value) => {
+        // Decycle the object
+        if (typeof value === "object" && value !== null) {
+            if (key === "instrument") {
+                return `[${key} ${value.name}]`;
+            }
+
+            if (key === "song") {
+                return `[this]`;
+            }
+
+            if (cache.includes(value)) {
+                return `[${key}]`;
+            }
+
+            cache.push(value);
+        }
+
+        return value;
+    }, 4);
+
+    postMessage(data);
+});
