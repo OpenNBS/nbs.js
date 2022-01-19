0e1d3a100d61b192cdcb50061dcc521297836153
GitHub Action <action@github.com>
2022-01-19 02:05:55 +0000

    deploy: 3933dfa865005877b905af449cb2c7672f2b1a67

--- demo/src/worker/highlight.js
+++ demo/src/worker/highlight.js
@@ -1,9 +1,1 @@
-import { highlightText } from "https://cdn.jsdelivr.net/gh/speed-highlight/core@1.1.4/dist/index.js";
-
-self.addEventListener("message", async event => {
-    const code = await highlightText(event.data.code, "json", false);
-
-    postMessage({
-        "code": code
-    });
-});
+import{highlightText}from"https://cdn.jsdelivr.net/gh/speed-highlight/core@1.1.4/dist/index.js";self.addEventListener("message",async e=>{e=await highlightText(e.data.code,"json",!1);postMessage({code:e})});
