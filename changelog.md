e6fd2ce3fa04c2f15a5814bc2adfbea561047b07
Encode42 <me@encode42.dev>
2022-03-06 18:50:44 -0500

    2.1.2

--- changelog.md
+++ changelog.md
@@ -4,6 +4,16 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+# 2.1.2
+### Changed
+- Updated all `devDependencies`.
+- Revamped build scripts.
+- Changed target from `ESNext` to `ES2015` for compatibility.
+
+### Fixed
+- Properly included typings.
+- Moved `devDependencies` out of `dependencies`.
+
 # 2.1.1
 ### Fixed
 - Fixed potential `defaultSongMeta` pollution.
--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,10 @@
-const typescript = require("@rollup/plugin-typescript");
-const { terser } = require("rollup-plugin-terser");
+import commonjs from "@rollup/plugin-commonjs";
+import nodeExternals from "rollup-plugin-node-externals";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import esbuild from "rollup-plugin-esbuild";
+import json from "@rollup/plugin-json";
+import dts from "rollup-plugin-dts";
+import path from "path";
 
 /**
  * The target output directory.
@@ -9,39 +14,40 @@ const { terser } = require("rollup-plugin-terser");
 const outDir = "dist";
 
 /**
- * Get an output configuration.
- *
- * @param {string} type Type to get output for
- * @param {boolean} [toMinify=false] Whether to create minified output
- * @return {Object}
- */
-function getOutput(type, toMinify = false) {
-    return {
-        "file": `${outDir}/${type}${toMinify ? ".min" : ""}.js`,
-        "name": type === "umd" ? "NBSjs" : undefined,
-        "format": type,
-        "plugins": toMinify ? [terser()] : undefined
-    };
-}
-
-/**
  * Get a base configuration
  *
  * @param {string} type Type to get base for
+ * @param {boolean} [toMinify=false] Whether to create minified output
  * @return {Object}
  */
-function getBase(type) {
-    return  {
+function getBase(type, toMinify = false) {
+    return {
         "input": "src/index.ts",
-        "output": [
-            getOutput(type, false),
-            getOutput(type, true)
-        ],
-        "plugins": [typescript({
-            "outDir": "dist"
-        })]
+        "output": {
+            "file": `${outDir}/${type}${toMinify ? ".min" : ""}.js`,
+            "name": type === "umd" ? "NBSjs" : undefined,
+            "format": type
+        },
+        "plugins": [
+            commonjs(),
+            nodeExternals(),
+            nodeResolve({
+                "extensions": [
+                    ".ts",
+                    ".js"
+                ]
+            }),
+            esbuild({
+                "minify": toMinify,
+                "sourceMap": false,
+                "tsconfig": path.resolve(process.cwd(), "tsconfig.json")
+            }),
+            json({
+                "compact": toMinify
+            })
+        ]
     };
-};
+}
 
 /**
  * Get the config for a build type.
@@ -50,11 +56,23 @@ function getBase(type) {
  * @return {Object[]}
  */
 function getConfig(...types) {
-    const configs = [];
+    const configs = [{
+        "input": "build/index.d.ts",
+        "output": {
+            "file": `${outDir}/index.d.ts`,
+            "format": "es"
+        },
+        "plugins": [
+            dts()
+        ]
+    }];
 
     // Generate config for each type
     for (const type of types) {
-        configs.push(getBase(type));
+        configs.push(
+            getBase(type),
+            getBase(type, true)
+        );
     }
 
     return configs;
