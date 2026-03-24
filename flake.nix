ced1ed5e77a24955f9a55f7fefd6e351a59459ef
encode42 <me@encode42.dev>
2026-03-24 19:12:03 -0400

    Include Nix development flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Nix development environment using Bun runtime and Biome formatter";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-25.11";
+
+  outputs =
+    { self, ... }@inputs:
+
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            inherit system;
+            pkgs = import inputs.nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs, system }:
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              bun
+              biome
+            ];
+          };
+        }
+      );
+    };
+}
