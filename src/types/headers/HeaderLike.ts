543b0301d1ebf01b8e7807d48ff633ffbdc3e025
encode42 <me@encode42.dev>
2026-04-18 04:19:45 -0400

    Convert HeaderLike into an abstract class

--- src/types/headers/HeaderLike.ts
+++ src/types/headers/HeaderLike.ts
@@ -11,16 +11,38 @@ import type {
 import type { StatisticsPiece } from "~/pieces/StatisticsPiece";
 import type { TempoPiece } from "~/pieces/TempoPiece";
 
-export type HeaderLike = {
-	"size": HeaderSize;
-	"version": MetadataVersion;
-	"name": OptionalMetadataName;
-	"author": OptionalMetadataAuthor;
-	"originalAuthor": OptionalMetadataAuthor;
-	"description": OptionalMetadataDescription;
-	"importName": OptionalMetadataImportName;
-	"autoSave": AutoSavePiece;
-	"loop": LoopPiece;
-	"statistics": StatisticsPiece;
-	"tempo": TempoPiece;
-};
+export abstract class HeaderLike {
+	public abstract size: HeaderSize;
+	public abstract version: MetadataVersion;
+	public abstract name: OptionalMetadataName;
+	public abstract author: OptionalMetadataAuthor;
+	public abstract originalAuthor: OptionalMetadataAuthor;
+	public abstract description: OptionalMetadataDescription;
+	public abstract importName: OptionalMetadataImportName;
+	public abstract autoSave: AutoSavePiece;
+	public abstract loop: LoopPiece;
+	public abstract statistics: StatisticsPiece;
+	public abstract tempo: TempoPiece;
+
+	public static assignHeader(fromHeader: HeaderLike, toHeader: HeaderLike): void {
+		toHeader.version = fromHeader.version;
+		toHeader.name = fromHeader.name;
+		toHeader.author = fromHeader.author;
+		toHeader.originalAuthor = fromHeader.originalAuthor;
+		toHeader.description = fromHeader.description;
+		toHeader.importName = fromHeader.importName;
+		toHeader.autoSave.isEnabled = fromHeader.autoSave.isEnabled;
+		toHeader.autoSave.interval = fromHeader.autoSave.interval;
+		toHeader.loop.isEnabled = fromHeader.loop.isEnabled;
+		toHeader.loop.count = fromHeader.loop.count;
+		toHeader.loop.startTick = fromHeader.loop.startTick;
+		toHeader.statistics.minutesSpent = fromHeader.statistics.minutesSpent;
+		toHeader.statistics.blocksAdded = fromHeader.statistics.blocksAdded;
+		toHeader.statistics.blocksRemoved = fromHeader.statistics.blocksRemoved;
+		toHeader.statistics.leftClicks = fromHeader.statistics.leftClicks;
+		toHeader.statistics.rightClicks = fromHeader.statistics.rightClicks;
+		toHeader.tempo.beats = fromHeader.tempo.beats;
+		toHeader.tempo.note = fromHeader.tempo.note;
+		toHeader.tempo.ticksPerSecond = fromHeader.tempo.ticksPerSecond;
+	}
+}
