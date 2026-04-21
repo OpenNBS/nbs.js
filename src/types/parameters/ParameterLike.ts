8194ceda215cdc2c07f7b01de7a9b8227932a2b9
encode42 <me@encode42.dev>
2026-04-21 02:40:48 -0400

    Despite all odds, static class methods are not read-only

--- src/types/parameters/ParameterLike.ts
+++ src/types/parameters/ParameterLike.ts
@@ -3,5 +3,5 @@ import type { Result } from "../validators/Result";
 export interface ParameterLike {
 	readonly "MAX_VALUE": unknown;
 	readonly "MIN_VALUE": unknown;
-	readonly "validate": (parameters: unknown) => Result;
+	"validate": (parameters: unknown) => Result;
 }
