ff87ee15fd02565a29f6379d1b40569f485e676c
encode42 <me@encode42.dev>
2026-04-20 23:08:48 -0400

    Make `ParameterLike` fields `readonly` rather than ignoring Biome rule

--- src/types/parameters/ParameterLike.ts
+++ src/types/parameters/ParameterLike.ts
@@ -1,9 +1,7 @@
 import type { Result } from "../validators/Result";
 
 export interface ParameterLike {
-	// biome-ignore-start lint/style/useNamingConvention: These are constant static fields
-	"MAX_VALUE": unknown;
-	"MIN_VALUE": unknown;
-	// biome-ignore-end lint/style/useNamingConvention: These are constant static fields
-	"validate": (parameters: unknown) => Result;
+	readonly "MAX_VALUE": unknown;
+	readonly "MIN_VALUE": unknown;
+	readonly "validate": (parameters: unknown) => Result;
 }
